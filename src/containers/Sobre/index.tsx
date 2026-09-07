8068bdccdcd4c9d2017f71ebe1d86e0bce533e08
VitorVT9 <vsilvab57@gmail.com>
2026-09-07 15:01:54 -0300

    feat: finaliza seção sobre mim com git status

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,16 @@ const Sobre = () => (
       repellat fugiat sed enim quia id dolor quas exercitationem doloremque
       debitis.
     </Paragrafo>
+    <GithubSecao>
+      <img
+        src="https://github-readme-stats-theta-five-48.vercel.app/api?username=VitorV9&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"
+        alt="Estatísticas do GitHub"
+      />
+      <img
+        src="https://github-readme-stats-theta-five-48.vercel.app/api/top-langs/?username=VitorV9&layout=compact&langs_count=7&theme=dracula"
+        alt="Linguagens mais usadas"
+      />
+    </GithubSecao>
   </section>
 )
 
